6a8222feab5560ff0ed50d2390f0a9f1da9ec5ad
taoqingqiu <taoqingqiu@gmail.com>
2021-09-06 13:22:30 +0800

    Make 'img' element used when exporting cross-origin

--- src/components/AvatarUploader.vue
+++ src/components/AvatarUploader.vue
@@ -18,12 +18,17 @@
         :style="{
           height: maskDiameter + 'px',
           width: maskDiameter + 'px',
-          'border-radius': maskShape === 'circle' ? '50%' : maskRectBorderRadius,
+          'border-radius':
+            maskShape === 'circle' ? '50%' : maskRectBorderRadius,
           'box-shadow': maskBoxShadow
         }"
         @mousedown="onMouseDown"
       >
-        <slot name="action.innerSelectButton" :on="{ click: toSelectImage }" v-if="!image">
+        <slot
+          name="action.innerSelectButton"
+          :on="{ click: toSelectImage }"
+          v-if="!image"
+        >
           <button @click="toSelectImage" v-if="!image">click to select</button>
         </slot>
       </div>
@@ -32,8 +37,13 @@
       <div class="action">
         <slot
           name="action.slider"
-          :attrs="{ min: minEnlargeTimes, max: maxEnlargeTimes, value: enlargeTimes, disabled: !image }"
-          :on="{ input: times => (enlargeTimes = times) }"
+          :attrs="{
+            min: minEnlargeTimes,
+            max: maxEnlargeTimes,
+            value: enlargeTimes,
+            disabled: !image
+          }"
+          :on="{ input: (times) => (enlargeTimes = times) }"
         >
           <input
             type="range"
@@ -58,23 +68,55 @@
             { value: 'circle', text: '圆形' },
             { value: 'square', text: '方形' }
           ]"
-          :on="{ input: shape => (maskShape = shape) }"
+          :on="{ input: (shape) => (maskShape = shape) }"
         >
           <label class="action-shape">
-            <input name="shape" :disabled="!image" type="radio" value="circle" checked v-model="maskShape" style="margin-right: 8px" />圆形
+            <input
+              name="shape"
+              :disabled="!image"
+              type="radio"
+              value="circle"
+              checked
+              v-model="maskShape"
+              style="margin-right: 8px"
+            />圆形
           </label>
           <label class="action-shape">
-            <input name="shape" :disabled="!image" type="radio" value="square" v-model="maskShape" style="margin-right: 8px" />方形
+            <input
+              name="shape"
+              :disabled="!image"
+              type="radio"
+              value="square"
+              v-model="maskShape"
+              style="margin-right: 8px"
+            />方形
           </label>
         </slot>
       </div>
 
       <div class="action">
-        <slot name="action.selectButton" :text="selectorText" :on="{ click: toSelectImage }">
-          <button type="button" class="action-button" @click="toSelectImage">{{ selectorText }}</button>
+        <slot
+          name="action.selectButton"
+          :text="selectorText"
+          :on="{ click: toSelectImage }"
+        >
+          <button type="button" class="action-button" @click="toSelectImage">
+            {{ selectorText }}
+          </button>
         </slot>
-        <slot name="action.confirmButton" :attrs="{ disabled: !image }" :on="{ click: confirm }">
-          <button type="button" class="action-button" @click="confirm" :disabled="!image">确定</button>
+        <slot
+          name="action.confirmButton"
+          :attrs="{ disabled: !image }"
+          :on="{ click: confirm }"
+        >
+          <button
+            type="button"
+            class="action-button"
+            @click="confirm"
+            :disabled="!image"
+          >
+            确定
+          </button>
         </slot>
       </div>
     </div>
@@ -82,7 +124,15 @@
 </template>
 <script>
 export default {
-  props: ['height', 'maskProps', 'outputProps', 'backgroundColor', 'maxMultiple', 'hideShapeGroup', 'img'],
+  props: [
+    "height",
+    "maskProps",
+    "outputProps",
+    "backgroundColor",
+    "maxMultiple",
+    "hideShapeGroup",
+    "img"
+  ],
   data() {
     return {
       image: null,
@@ -95,7 +145,7 @@ export default {
       bgImageSrcPostion: [0, 0],
       bgStartPos: [],
       enlargeTimes: 1,
-      maskShape: 'circle'
+      maskShape: "circle"
     };
   },
   computed: {
@@ -103,19 +153,21 @@ export default {
       return this.height || 150;
     },
     selectorText() {
-      return !this.image ? '选择' : '重选';
+      return !this.image ? "选择" : "重选";
     },
     maskBoxShadowColor() {
-      return (this.maskProps && this.maskProps.shadowColor) || '#00000025';
+      return (this.maskProps && this.maskProps.shadowColor) || "#00000025";
     },
     maskRectBorderRadius() {
-      return (this.maskProps && this.maskProps.squareBorderRadius) || '5px';
+      return (this.maskProps && this.maskProps.squareBorderRadius) || "5px";
     },
     maskBoxShadow() {
-      return `${this.maskTransparent ? this.maskBoxShadowColor : '#fff'} 0 0 0 ${this.maskBoxShadowRadius}px`;
+      return `${
+        this.maskTransparent ? this.maskBoxShadowColor : "#fff"
+      } 0 0 0 ${this.maskBoxShadowRadius}px`;
     },
     adjusterBgColor() {
-      return this.backgroundColor || '#f2f3f8';
+      return this.backgroundColor || "#f2f3f8";
     },
     adjusterBgPosi() {
       return `${this.bgImagePostion[0]}px ${this.bgImagePostion[1]}px`;
@@ -129,16 +181,21 @@ export default {
       return (xRatio > yRatio ? xRatio : yRatio) || 0.5;
     },
     bgImageSize() {
-      return [this.bgImageSrcSize[0] * this.enlargeTimes, this.bgImageSrcSize[1] * this.enlargeTimes];
+      return [
+        this.bgImageSrcSize[0] * this.enlargeTimes,
+        this.bgImageSrcSize[1] * this.enlargeTimes
+      ];
     },
     distSize() {
       return (this.outputProps && this.outputProps.outputSize) || [100, 100];
     },
     distFormat() {
-      return (this.outputProps && this.outputProps.format) || 'png';
+      return (this.outputProps && this.outputProps.format) || "png";
     },
     distBackgroundColor() {
-      return (this.outputProps && this.outputProps.backgroundColor) || 'transparent';
+      return (
+        (this.outputProps && this.outputProps.backgroundColor) || "transparent"
+      );
     }
   },
   mounted() {
@@ -146,11 +203,11 @@ export default {
       // 宽高、img 初始化
       this.initialize();
       // 绑一个必要的 mouseUp 事件
-      document.addEventListener('mouseup', this.onMouseUp);
+      document.addEventListener("mouseup", this.onMouseUp);
     }, 200);
   },
   beforeDestroy() {
-    document.removeEventListener('mouseup', this.onMouseUp);
+    document.removeEventListener("mouseup", this.onMouseUp);
   },
   methods: {
     onMouseDown(event) {
@@ -160,7 +217,7 @@ export default {
         this.maskTransparent = true;
 
         // move 事件
-        this.$refs.adjuster.addEventListener('mousemove', this.onMouseMove);
+        this.$refs.adjuster.addEventListener("mousemove", this.onMouseMove);
       }
     },
     onMouseMove(event) {
@@ -168,7 +225,10 @@ export default {
         deltaY = event.y - this.bgStartPos[1];
 
       // 更新 bg image 位置
-      this.bgImagePostion = [this.bgImagePostion[0] + deltaX, this.bgImagePostion[1] + deltaY];
+      this.bgImagePostion = [
+        this.bgImagePostion[0] + deltaX,
+        this.bgImagePostion[1] + deltaY
+      ];
       this.bgStartPos = [event.x, event.y];
     },
     onMouseUp() {
@@ -177,22 +237,22 @@ export default {
         this.maskTransparent = false;
 
         // remove move 事件
-        this.$refs.adjuster.removeEventListener('mousemove', this.onMouseMove);
+        this.$refs.adjuster.removeEventListener("mousemove", this.onMouseMove);
       }
     },
     toSelectImage() {
-      const input = document.createElement('input');
-      input.type = 'file';
-      input.accept = 'image/*';
+      const input = document.createElement("input");
+      input.type = "file";
+      input.accept = "image/*";
       input.click();
-      input.onchange = e => {
+      input.onchange = (e) => {
         this.enlargeTimes = 1;
 
         const targetImage = e.target.files[0];
         targetImage && (this.image = URL.createObjectURL(targetImage));
 
         // adjust image to fit container
-        const img = document.createElement('img');
+        const img = document.createElement("img");
         img.src = this.image;
         img.onload = this.imgOnload;
       };
@@ -203,18 +263,28 @@ export default {
         yRatio = this.containerShape[1] / height;
       if (xRatio > yRatio) {
         this.bgImageSrcSize = [width * xRatio, height * xRatio];
-        this.bgImagePostion = [0, (-height * xRatio + this.containerShape[1]) / 2];
+        this.bgImagePostion = [
+          0,
+          (-height * xRatio + this.containerShape[1]) / 2
+        ];
       } else {
         this.bgImageSrcSize = [width * yRatio, height * yRatio];
-        this.bgImagePostion = [(-width * yRatio + this.containerShape[0]) / 2, 0];
+        this.bgImagePostion = [
+          (-width * yRatio + this.containerShape[0]) / 2,
+          0
+        ];
       }
     },
     initialize() {
       const { offsetHeight, offsetWidth } = this.$refs.adjuster;
 
       // mask
-      this.maskDiameter = offsetHeight < offsetWidth ? offsetHeight : offsetWidth;
-      this.maskBoxShadowRadius = (Math.pow(offsetWidth ** 2 + offsetHeight ** 2, 0.5) - this.maskDiameter) / 2;
+      this.maskDiameter =
+        offsetHeight < offsetWidth ? offsetHeight : offsetWidth;
+      this.maskBoxShadowRadius =
+        (Math.pow(offsetWidth ** 2 + offsetHeight ** 2, 0.5) -
+          this.maskDiameter) /
+        2;
 
       // adjuster
       this.containerShape = [offsetWidth, offsetHeight];
@@ -222,30 +292,33 @@ export default {
       // img
       if (this.img) {
         this.image = this.img;
-        const img = document.createElement('img');
+        const img = document.createElement("img");
         img.src = this.img;
         img.onload = this.imgOnload;
       }
     },
     confirm() {
-      const img = document.createElement('img');
+      const img = document.createElement("img");
+      img.setAttribute("crossorigin", "anonymous");
       img.src = this.image;
 
       const vm = this;
-      img.onload = function() {
+      img.onload = function () {
         vm.cropImage(this);
       };
     },
     cropImage(img) {
-      img.setAttribute('crossorigin', 'anonymous');
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
       canvas.width = this.distSize[0];
       canvas.height = this.distSize[1];
 
       // background
-      if (this.distBackgroundColor === 'transparent' && this.distFormat !== 'png') {
-        ctx.fillStyle = '#fff';
+      if (
+        this.distBackgroundColor === "transparent" &&
+        this.distFormat !== "png"
+      ) {
+        ctx.fillStyle = "#fff";
       } else {
         ctx.fillStyle = this.distBackgroundColor;
       }
@@ -257,8 +330,12 @@ export default {
       const ratio = width / this.bgImageSize[0];
       ctx.drawImage(
         img,
-        (-this.bgImagePostion[0] + (this.containerShape[0] - this.maskDiameter) / 2) * ratio,
-        (-this.bgImagePostion[1] + (this.containerShape[1] - this.maskDiameter) / 2) * ratio,
+        (-this.bgImagePostion[0] +
+          (this.containerShape[0] - this.maskDiameter) / 2) *
+          ratio,
+        (-this.bgImagePostion[1] +
+          (this.containerShape[1] - this.maskDiameter) / 2) *
+          ratio,
         this.maskDiameter * ratio,
         this.maskDiameter * ratio,
         0,
@@ -266,7 +343,7 @@ export default {
         this.distSize[0],
         this.distSize[1]
       );
-      this.$emit('confirm', canvas.toDataURL(`image/${this.distFormat}`));
+      this.$emit("confirm", canvas.toDataURL(`image/${this.distFormat}`));
     }
   }
 };
